9c9c03911715cf1a714a37cc5f1a0a241363ee7c
Ravit Dennis <rdennis@codeaurora.org>
2013-12-10 12:44:49 +0200

    msm: ultrasound: Add Sync Gesture data format
    A new Sync Gesture service has been added to userspace and QDSP6.
    This service data format should be supported in the ultrasound
    related definitions in the kernel.
    
    Change-Id: I99dcaa74915c73ffb7329fb88226866b79a7ef73
    Signed-off-by: Ravit Dennis <rdennis@codeaurora.org>
    Signed-off-by: poondog <markj338@gmail.com>

--- include/sound/apr_audio-v2.h
+++ include/sound/apr_audio-v2.h
@@ -6175,5 +6175,7 @@ struct asm_eq_params {
 #define US_POINT_EPOS_FORMAT_V2 0x0001272D
 #define US_RAW_FORMAT_V2        0x0001272C
 #define US_PROX_FORMAT_V2       0x0001272E
+#define US_RAW_SYNC_FORMAT      0x0001272F
+#define US_GES_SYNC_FORMAT      0x00012730
 
 #endif /*_APR_AUDIO_V2_H_ */
